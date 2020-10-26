b8563f7e857b8e0f23b0f59667244ac879ac1796
Ola Tomczyk <ola443@op.pl>
2020-10-26 21:59:16 +0100

    indexjs changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+import moment from "moment";
+
+const now = moment();
+const nowShort = now.format("MM-DD-YYYY");
+const nowLong = now.format("DD MM YYYY hh:mm:ss");
+
+console.log(nowShort, nowLong);
+
+console.log("good night");
